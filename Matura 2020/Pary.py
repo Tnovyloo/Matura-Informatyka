9de23c0a03e84b747d6f1ccbe17a4518b7841d4b
Tomasz <loobrome@gmail.com>
2022-01-04 14:41:50 +0100

    Adding 2020 Matura

---
+++ Matura 2020/Pary.py
@@ -0,0 +1,52 @@
+import math
+
+lista = []
+with open('pary.txt') as file:
+    for line in file:
+        lista.append(line.strip())
+
+def CzyPierwsza(n):
+    s = int(math.sqrt(n))
+    for i in range(2, s + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def GoldBach(n):
+    if n % 2 == 0:
+        return
+    num1 = 0
+    num2 = 0
+    for i in range(3, n + 1):
+        r = n - 1
+        if r % 2 == 0 or i % 2 == 0:
+            continue
+        if not CzyPierwsza(r) or not CzyPierwsza(i):
+            continue
+        if num2 - num1 <= r - i:
+            num2 = r
+            num1 = i
+    print(f'{n} {num1} {num2}')
+
+for i in lista:
+    num = int(i.split(" ")[0])
+    GoldBach(num)
+
+word = ""
+for line in lista:
+    word = line.split(" ")[1]
+    longest = ""
+    current = ""
+    for i in range(len(word)):
+        if len(current) == 0 or current[0] == word[i]:
+            current += word[i]
+        else:  # w przeciwnym wypadku
+            if len(current) > len(longest):
+                longest = current
+            current = ""
+            current += word[i]
+    if len(current) > len(longest):
+        longest = current
+    if len(longest) == 0:
+        longest = current
+    print(longest + " " + str(len(longest)))
