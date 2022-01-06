a77e11a9992f130fd06062564a7409adecf847bd
Tomasz <loobrome@gmail.com>
2022-01-06 12:54:52 +0100

    Adding finding geometrical sequence alghoritm.

--- Potrzebne Algorytmy/Szukanie_Ciagu_w_liscie.py
+++ Potrzebne Algorytmy/Szukanie_Ciagu_w_liscie.py
@@ -1,45 +1,64 @@
 from random import randint
 
 def generate_random_list(array_len, minimal, maximal):
+    """Generate random list"""
     user_array = []
     for num in range(array_len):
         user_array.append(randint(minimal, maximal))
     return user_array
 
-def znajdz_ciag(user_arr):
+def find_number_sequence(user_arr):
+    """Finding number sequence in list"""
     temp = 1
-    max_dlugosc_ciagu = 1
-    liczba_w_ciagu = ''
+    max_len_sequence = 1
+    number_sequence = ''
     for num in range(len(user_arr)-1):
-        if temp > max_dlugosc_ciagu:
-            max_dlugosc_ciagu = temp
-            liczba_w_ciagu = user_arr[num]
+        if temp > max_len_sequence:
+            max_len_sequence = temp
+            number_sequence = user_arr[num]
 
         if user_arr[num] == user_arr[num+1]:
-            # liczba_w_ciagu = str(user_arr[num]) #to nie moze tutaj byc bo
-            # jezeli pod koniec list beda chociaz dwa elementy takie same to
-            # nasz program zapisuje liczbe do zmiennej
             temp += 1
 
         if user_arr[num] != user_arr[num+1]:
             temp = 1
 
-    return liczba_w_ciagu, max_dlugosc_ciagu
+    return number_sequence, max_len_sequence
 
+def find_geometrical_seq(user_arr):
+    """Find geometrical sequence in list"""
+    sequence = []
+    for num in range(len(user_arr)-1):
+        if user_arr[num] * user_arr[num] != user_arr[num-1] * user_arr[num+1]:
+            continue
+        else:
+            sequence.append(user_arr[num])
+    return sequence
 
-arr = generate_random_list(10, 1, 2)
+def find_geometrical_seq_ver2(user_arr):
+    sequence = []
+    for num in range(len(user_arr)-1):
+        q = user_arr[num] / user_arr[num]
+        if user_arr[num + 1] / user_arr[num - 1] == q:
+            sequence.append(user_arr[num])
+        else:
+            continue
+    return sequence
 
-# PROBLEMATIC ARRAYS TO MY ALGHORITM
+arr = generate_random_list(100, 1, 10)
+
+# PROBLEMATIC ARRAYS TO MY FIND SEQUENCE ALGHORITM
 # arr = [2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2]
 # arr = [1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1]
 # arr = [1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]
 # arr = [2, 2, 1, 1, 1, 1, 1, 1, 2, 2]
+#NOW IS WORKING
 
-# seq = find_sequence(arr)
-# char 2, len(maximal) 8
-
-seq = znajdz_ciag(arr)
-
+seq = find_number_sequence(arr)
 print(f"liczba w ciagu {seq[0]}, dlugosc ciagu {seq[1]}")
 
-#TODO szukaj ciągu geometrycznego
+geometrical_seq = find_geometrical_seq(arr)
+print(geometrical_seq)
+
+var = find_geometrical_seq_ver2(arr)
+print(var)
