e32eef37a221986549b373219411cd10e82f369a
Tomasz <loobrome@gmail.com>
2022-01-11 17:10:06 +0100

    Matura 2021 - Adding complete task

--- Matura 2021/Instrukcje.py
+++ Matura 2021/Instrukcje.py
@@ -3,8 +3,6 @@ with open('instrukcje.txt', 'r') as file:
     for line in file:
         instrukcje.append(line.strip())
 
-# print(lista)
-
 def zadanie4_1(lista):
     dlugosc = 0
     for var in lista:
@@ -13,7 +11,6 @@ def zadanie4_1(lista):
         if var.__contains__('USUN'):
             dlugosc -= 1
     return dlugosc
-
 
 def zadanie4_2(lista):
     max = 0
@@ -30,35 +27,7 @@ def zadanie4_2(lista):
             temp = 1
     return max, text
 
-    # best = ['start', 0]
-    # poprzedni = 'start'
-    # dlugosc = 0
-    # for idx, item in enumerate(lista):
-    #     krok = item.split()[0]
-    #     if krok == poprzedni:
-    #         dlugosc += 1
-    #     else:
-    #         if dlugosc > best[1]:
-    #             best[0] = lista[idx - 1].split()[0]
-    #             best[1] = dlugosc
-    #         poprzedni = krok
-    #         dlugosc = 1
-    # return best
-
 def zadanie4_3(lista):
-    # char = ''
-    # temp = 1
-    # max = 0
-    # for var in lista:
-    #     if var.__contains__('DOPISZ'):
-    #         if temp > max:
-    #             temp += 1
-    #             char = var.split()[1]
-    #             max = temp
-    #         else:
-    #             temp = 0
-    # return char, max
-
     litery = {}
     for var in lista:
         rodzaj, znak = var.split()
@@ -75,13 +44,52 @@ def zadanie4_3(lista):
             najczesciej = litery[litera]
     # skrócona wersja powyższego dla ciekawych:
     # najczesciej = max(litery.items(), key=lambda x: x[1])
-    print("3)", najczesciej_litera, najczesciej)
+    return najczesciej_litera, najczesciej
+
+def dopisz(tekst, litera):
+    return tekst + litera
+
+def usun(tekst):
+    return tekst[:len(tekst)-1]
+
+def zmien(tekst, litera): #funckja zmieniajaca ostatni znak
+    txtlist = list(tekst) #uwaga: w pythonie teksty musimy potraktowac jako liste
+    txtlist[len(tekst) - 1] = litera #jezeli chcemy podmienic znak na danej pozycji
+    return "".join(txtlist) #na koniec liste ta musimy zmienic z powrotem na tekst
+
+def przesun(tekst, litera):
+    index = tekst.find(litera)
+    if index == -1:  # jezeli nie znalezlismy znaku, przerywamy dzialanie
+        return tekst
+    if litera == 'Z':  # jezeli jest to ostatnia litera alfabetu, to wracamy na poczatek
+        litera = 'A'
+    else:  # w przeciwnym przypadku przesuwamy znak o 1
+        litera = chr(ord(litera) + 1)
+    txtlist = list(tekst)  # nastepnie musimy ponownie zamienic text na liste znakow
+    txtlist[index] = litera  # podmienic znak
+    return "".join(txtlist)  # i zwrocic ja jako ciag znakow
 
 
+def zadanie4_4(lista):
+    result = ''
+    for var in lista:
+        args = var.split(" ")
+        char = args[1]
+        command = args[0]
+        if command == "DOPISZ":
+            result = dopisz(result, char)
+        if command == "ZMIEN":
+            result = zmien(result, char)
+        if command == "USUN":
+            result = usun(result)
+        if command == "PRZESUN":
+            result = przesun(result, char)
+    return result
 
 print(zadanie4_1(instrukcje))
 print(zadanie4_2(instrukcje))
 print(zadanie4_3(instrukcje))
+print(zadanie4_4(instrukcje))
 
 #TODO ZADANIE 4_4
 
