190a5092f4bbd0eb019477397abe9e4900c76514
Tomasz <loobrome@gmail.com>
2022-01-13 18:13:47 +0100

    Eratostenes alghoritm

--- Potrzebne Algorytmy/Nwd.py
+++ Potrzebne Algorytmy/Nwd.py
@@ -1,17 +1,6 @@
-# Największy wspólny dzielnik (NWD) dla dwóch liczb jest to największa liczba naturalna,
-# która dzieli każdą z tych liczb bez reszty.
-#
-# Na przykład dla 16 i 24,
-# największą liczbą naturalną która dzieli
-# te dwie liczby bez reszty jest 8.
-#
-# Dla liczb 3 i 9 jest to 3.
-#
-# Natomiast dla liczb 3 i 7 NWD będzie wynosić 1
-
 #Funkcja rekurencyjna przyjmuje dwa parametry
 def nwd(a, b):
-    #Jesli b > 0 wtedy wywołaj funkcję z parametrami powyżej
+    #Jesli b > 0 wtedy wywołaj funkcję z parametrami
     if b > 0:
         #Parametr a%b to reszta z dzielenia
         return nwd(b, a%b)
