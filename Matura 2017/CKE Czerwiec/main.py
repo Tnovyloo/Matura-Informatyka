8c5f17cbec1c1fd123b3a8e9860a0fa64977019e
Tnovyloo <76777800+Tnovyloo@users.noreply.github.com>
2022-04-07 16:52:55 +0200

    CKE 2017 Czerwiec - ended :)

--- Matura 2017/CKE Czerwiec/main.py
+++ Matura 2017/CKE Czerwiec/main.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 with open('punkty.txt', 'r') as file:
     data_file = [line.rstrip().split() for line in file]
 
@@ -21,6 +23,10 @@ def anagram(numbers):
         return True
     return False
 
+def distance_of_points(a, b):
+    distance = round(sqrt((pow(int(b[0]) - int(a[0]), 2) + pow(int(b[1]) - int(a[1]), 2))), 0)
+    return distance
+
 def find_primes(data):
     primes = []
     for numbers in data:
@@ -32,7 +38,43 @@ def find_anagrams(data):
     anagrams = [numbers for numbers in data if anagram(numbers)]
     return anagrams
 
+def find_longest_distance(data):
+    maximal_distance = 0
+    p1 = []
+    p2 = []
+    for point_a in data:
+        for point_b in data:
+            difference = distance_of_points(point_a, point_b)
+            if difference > maximal_distance:
+                maximal_distance = difference
+                if not p1 and not p2:
+                    p1.append(point_a)
+                    p2.append(point_b)
+                else:
+                    p1.clear()
+                    p2.clear()
+                    p1.append(point_a)
+                    p2.append(point_b)
+    return p1, p2, maximal_distance
+
+def check_points_in_square(data):
+    interior = 0
+    exterior = 0
+    on_border = 0
+    for point in data:
+        if int(point[0]) < 5000 and int(point[1]) < 5000:
+            interior += 1
+        if int(point[0]) > 5000 or int(point[1]) > 5000:
+            exterior += 1
+        if int(point[0]) == 5000 or int(point[1]) == 5000:
+            on_border += 1
+    return f'In square {interior}\nOut of square {exterior}\nOn border {on_border} '
+
 #4.1
 print(len(find_primes(data_file)))
 #4.2
 print(len(find_anagrams(data_file)))
+#4.3
+print(find_longest_distance(data_file))
+#4.4
+print(check_points_in_square(data_file))
